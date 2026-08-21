d83506b9956ba91b43782f9e13a69801ee3aaf63
reedaa01 <redarguila@gmail.com>
2026-08-21 20:56:52 +0100

    add ^public subnet

--- 02-reseau-vnet-networking/main.tf
+++ 02-reseau-vnet-networking/main.tf
@@ -16,3 +16,9 @@ resource "azurerm_virtual_network" "terraform_dev_rg" {
   location            = "West Europe"
   address_space       = ["10.0.0.0/16"]
 }
+resource "azurerm_subnet" "terraform_dev_rg" {
+  name                 = "terraform-public-subnet"
+  resource_group_name  = "terraform-dev-rg"
+  virtual_network_name = "terraform-vnet"
+  address_prefixes     = ["10.0.1.0/24"]
+}
